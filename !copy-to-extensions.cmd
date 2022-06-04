e13a8bb6e71849c8b559368f781745450fc2d346
Nikita Zimin <nzeemin@gmail.com>
2022-06-05 01:29:11 +0300

    Initial commit.

---
+++ !copy-to-extensions.cmd
@@ -0,0 +1,2 @@
+del C:\Users\nzeem\.vscode\extensions\skoolkit-ctl-syntax\*.* /Q
+xcopy . C:\Users\nzeem\.vscode\extensions\skoolkit-ctl-syntax /E /Y
---
+++ README.md
@@ -0,0 +1,3 @@
+# skoolkit-ctl-syntax README
+
+This extension provides basic syntax highlighting for Skoolkit Control files (.ctl files).
